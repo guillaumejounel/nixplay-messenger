06a2ae37d3a84de5b117d11c3bedca48dcc7eb93
Guillaume Jounel <gjounel@gmail.com>
2019-06-04 22:46:03 +1000

    Implemented logging back from session

--- src/utils/facebook.js
+++ src/utils/facebook.js
@@ -1,17 +1,43 @@
 const login = require("facebook-chat-api");
+const fs = require("fs");
 
 const credentials = {
   email: process.env.FB_USR,
   password: process.env.FB_PWD
 };
 
-const getApi = () => {
+const reLogIn = () => {
+  return new Promise((resolve, reject) => {
+    login(
+      { appState: JSON.parse(fs.readFileSync("appstate.json", "utf8")) },
+      (err, api) => {
+        if (err) reject(err);
+        console.log("Logged back from previous session.");
+        resolve(api);
+      }
+    );
+  });
+};
+
+const logIn = () => {
   return new Promise((resolve, reject) => {
     login(credentials, (err, api) => {
       if (err) reject(err);
-      api.setOptions({ selfListen: true });
+      fs.writeFileSync("appstate.json", JSON.stringify(api.getAppState()));
       resolve(api);
     });
+  });
+};
+
+const getApi = () => {
+  return new Promise((resolve, reject) => {
+    reLogIn()
+      .then(api => resolve(api))
+      .catch(() => {
+        logIn()
+          .then(api => resolve(api))
+          .catch(err => reject(err));
+      });
   });
 };
 
